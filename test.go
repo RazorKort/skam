f356710280e63fca5f7de03b08df038867fb48ff
Razor_kort <55944415+RazorKort@users.noreply.github.com>
2026-03-11 19:04:56 +0700

    Add files via upload

--- test.go
+++ test.go
@@ -1,22 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"image/color"
+	"log"
+	"os"
+
+	"gioui.org/app"
+	"gioui.org/op"
+	"gioui.org/text"
+	"gioui.org/widget/material"
+)
 
 func main() {
-	password := "123"
-	path := "session.key"
-	client, err := NewClient("https://skam.su:10000")
-	if err != nil {
-		print("hui")
+	go func() {
+		window := new(app.Window)
+		err := run(window)
+		if err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}()
+	app.Main()
+}
+
+func run(window *app.Window) error {
+	theme := material.NewTheme()
+	var ops op.Ops
+	for {
+		switch e := window.Event().(type) {
+		case app.DestroyEvent:
+			return e.Err
+		case app.FrameEvent:
+			// This graphics context is used for managing the rendering state.
+			gtx := app.NewContext(&ops, e)
+
+			// Define an large label with an appropriate text:
+			title := material.H1(theme, "Hello, Gio")
+
+			// Change the color of the label.
+			maroon := color.NRGBA{R: 127, G: 0, B: 0, A: 255}
+			title.Color = maroon
+
+			// Change the position of the label.
+			title.Alignment = text.Middle
+
+			// Draw the label to the graphics context.
+			title.Layout(gtx)
+
+			// Pass the drawing operations to the GPU.
+			e.Frame(gtx.Ops)
+		}
 	}
-	LoadKeys(client, path, password)
-	err = Auth(client)
-	if err != nil {
-		fmt.Println(err)
-	}
-	friends, err := GetFriends(client)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(friends)
 }
